5732e45f48a1933d22c83ae4a08e911d9a723671
David Nichols <davenich@gmail.com>
2016-08-13 15:23:46 -0400

    idk

--- app.js
+++ app.js
@@ -11,19 +11,20 @@ app.listen(process.env.PORT || 12345, () => {
   console.log('Server started.');
 });
 
+app.use(cors());
+
+// // Add headers
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     // res.header('Access-Control-Allow-Methods', 'POST');
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+
 // API parent route
 let api = express.Router({ mergeParams: true });
 
 app.use(config.API_ROOT, api);
-app.use(cors());
-
-// Add headers
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    // res.header('Access-Control-Allow-Methods', 'POST');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 /**
  * API endpoints
